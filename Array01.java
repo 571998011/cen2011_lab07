85b946d23f8d0c336818cd22ab1da023a2fed205
My Life for Aiur <thanapoom.son@crru.ac.th>
2015-09-25 14:30:57 +0700

    update java file ...

--- Array01.java
+++ Array01.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Array01 {
   public static void main (String args[]) {
-      int[] a;
+	  Scanner scan = new Scanner(System.in);
+	  int n=scan.nextInt();
+      int[] a=new int[n];
+	  for(int i=1;i<=a.length;i++)
+	  System.out.println("A = "+i);
       
       // Assign Value to A
       
